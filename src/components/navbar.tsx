1b247fad1fb06ddc3a333874fc54e10cec7b101b
nepatyres <contactjulijus@gmail.com>
2024-07-14 21:36:29 +0300

    update

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -3,7 +3,6 @@ import React from "react";
 export default function Navbar() {
     return (
         <nav className="fixed h-16 top-5 w-[95%] bg-white/25 rounded-3xl z-50 left-1/2 transform -translate-x-1/2">
-
         </nav>
     )
 };
