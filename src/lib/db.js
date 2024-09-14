a9101c2fb11275519d47b9904f88da1048adea46
nepatyres <contactjulijus@gmail.com>
2024-09-14 03:50:05 +0300

    daily update

--- src/lib/db.js
+++ src/lib/db.js
@@ -1,9 +1,9 @@
 import { Sequelize } from 'sequelize';
 
-const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
-    host: process.env.DB_HOST,
+const sequelize = new Sequelize('artisto', 'julijus', 'GddpndpXzZrja+', {
+    host: '35.202.159.100',
     dialect: 'mysql',
-    port: process.env.DB_PORT,
+    port: 3306,
 });
 
 export default sequelize;
